187b132384382a292035f5f25d0fac1c5979f943
Justin Ho <hochungyan@live.hk>
2022-03-31 17:59:43 +0100

    good change

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp8
                 float[] Walls = new float[nOW];
 
 
+                int lit2 = 0;
+                float R = 0;
                 for (int A = 0; A < nOW; A++)
                 {
                     Console.WriteLine("Input Your Length in meters For Wall " + (A + 1));
@@ -44,8 +46,24 @@ namespace ConsoleApp8
                     {
                         case "black":
                             cOP = 24;
-                            Console.WriteLine("Black Colour Can Will Cost £24 each");
+                            Console.WriteLine("2.5L Can Will Cost £24 each and 5L Can will Cost 48 each");
+                            Console.WriteLine("which Lit can do you want");
+                            float lit1= float.Parse(Console.ReadLine());
 
+                            if (lit1 == 2.5)
+                            { 
+                                 lit2 = 24;
+                                R = (Walls[A] / 35);
+                            }
+
+                            else if (lit1 == 5)
+                            { 
+                                lit2 = 48;
+                                R = (Walls[A] / 70);
+                            }
+                            else
+                            { Console.WriteLine("Invalid"); }
+                        
                             break;
 
                         case "red":
@@ -75,14 +93,17 @@ namespace ConsoleApp8
                             break;
 
                     }
+                    //2.5L covers 35m^2, 5L covers 70m^2
 
-                    float R = (Walls[A]/ 35);
+                
                     double R2 = Math.Round(R);
                     double cost = R2 * cOP;
+                    double cost2 = R2 * lit2;
                     Console.WriteLine("The coverage for 2.5 Lit Can is 35m^2");
                     Console.WriteLine("Required Paint For Wall " + (A + 1) + ": " + R +" Units of 2.5 Litre Can");
                     Console.WriteLine("Total Required Paint For Wall "  +(A +1) + ": "+ Math.Round(R) + " Units of 2.5 Litre Can");
                     Console.WriteLine("Cost : " + "£" + cost);
+                    Console.WriteLine("Cost : " + "£" + cost2);
 
                 }
 
