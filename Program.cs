bb98ddef79c3f0c306269b849a6c1a894644ee12
Justin Ho <hochungyan@live.hk>
2022-03-31 10:51:31 +0100

    Day 2 Change

--- Program.cs
+++ Program.cs
@@ -10,27 +10,62 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input Your Length in m");
+            //Wall 1
+            Console.WriteLine("Input Your Length in meters For Wall 1");
             int Length = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Your Width in m");
+            Console.WriteLine("Input Your Width in meters For Wall 1");
             int Width = Convert.ToInt32(Console.ReadLine());
-            float Area = (Length * Width);
-            Console.WriteLine("Total Area : " + Area +"m^2");
+            int Area = (Length * Width);
+            
+            //Wall 2
+            Console.WriteLine("Input Your Length in meters For Wall 2");
+            int Length2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Input Your Width in meters For Wall 2");
+            int Width2 = Convert.ToInt32(Console.ReadLine());
+            int Area2 = (Length2 * Width2);
+
+            int[] Walls = new int[2];
+            Walls[0] = Area;
+            Walls[1] = Area2;
+
+            
+            for (int i = 0; i< Walls.Length;i++)
+            {
+                Console.WriteLine("Total Area for Walls : ");
+                Console.WriteLine(Walls[i]);
+            }
+
+
+
+
+            Console.WriteLine("Total Area For Wall 1: " + Area +"m^2");
             //Paint covers 25square meter
             float Paint = 25;
             float Required = (Area / Paint);
             //Paint Cost £5
             float Cost = 5;
             Console.WriteLine("Required Paint : " + Math.Round(Required));
+
+
+            Console.WriteLine("Total Area For Wall 2 : " + Area2 + "m^2");
+            //Paint covers 25square meter
+           
+            float Required2 = (Area2 / Paint);
+            //Paint Cost £5
             
+            Console.WriteLine("Required Paint : " + Math.Round(Required2));
+
+
+
+
             int workerFee = 100;
             if (Area >= 100)
 
             {
-                Console.WriteLine("Paint Cost : £" + ((Math.Round(Required) * Cost)));
-                Console.WriteLine("Total Cost : £" + ((Math.Round(Required) * Cost) + workerFee));
+                Console.WriteLine("Paint Cost For Wall 1: £" + ((Math.Round(Required) * Cost)));
+                Console.WriteLine("Total Cost For Wall 1: £" + ((Math.Round(Required) * Cost) + workerFee));
 
-                Console.WriteLine("You will Need to hire an expert "); }
+                Console.WriteLine(" [Result: You will Need to hire an expert] "); }
            else if (Area >= 40)
             { Console.WriteLine("You can do it with your friends");
                 Console.WriteLine("Paint Cost : £" + ((Math.Round(Required) * Cost)));
@@ -41,6 +76,33 @@ namespace ConsoleApp8
             {
                 Console.WriteLine("Paint Cost : £" + ((Math.Round(Required) * Cost)));
                 Console.WriteLine("You can do it Yourself"); }
+
+            
+            if (Area2 >= 100)
+
+            {
+                Console.WriteLine("Paint Cost For Wall 2: £" + ((Math.Round(Required2) * Cost)));
+                Console.WriteLine("Total Cost For Wall 2: £" + ((Math.Round(Required2) * Cost) + workerFee));
+
+                Console.WriteLine(" [Result: You will Need to hire an expert] ");
+            }
+            else if (Area2 >= 40)
+            {
+                Console.WriteLine("You can do it with your friends (Wall 2)");
+                Console.WriteLine("Paint Cost : £" + ((Math.Round(Required2) * Cost)));
+            }
+            else if (Area2 == 0)
+            { Console.WriteLine("Invalid"); }
+            else
+            {
+               Console.WriteLine("Paint Cost : £" + ((Math.Round(Required2) * Cost)));
+              
+               Console.WriteLine("You can do it Yourself (Wall2)");
+            }
+
+
+
+
 
             Console.ReadKey();
         }
